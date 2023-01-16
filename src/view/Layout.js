393c82297f09e8f09743cfc91ea5df99a1216696
Titanstic <mgminkhant301999@gmail.com>
2023-01-16 21:30:09 +0630

    layout beta

--- src/view/Layout.js
+++ src/view/Layout.js
@@ -8,14 +8,14 @@ const Layout = ({children}) => {
     return (
         <div className="h-screen">
             <div className="flex">
-                <div className="w-56 h-screen bg-teal-600 text-white shadow-2xl">
+                <div className="w-56 h-screen bg-teal-500 text-white shadow-2xl shadow-gray-500">
                     <h1 className="text-4xl font-bold text-center py-3">Soccer 22</h1>
 
                     <hr/>
 
                     <div className="w-24 text-xl mt-5 mx-auto">
-                        <Link to="/" className={`block mb-3 ${navActive === 'home' && "bg-blue-400"}`} onClick={() => setNavActive('home')}>Home</Link>
-                        <Link to="/user" className="block mb-3" onClick={() => setNavActive('user')}>User</Link>
+                        <Link to="/" className={`inline-block rounded-md mb-3 px-4 py-2 ${navActive === 'home' && "bg-gray-500"}`} onClick={() => setNavActive('home')}>Home</Link>
+                        <Link to="/user" className="inline-block mb-3 px-4 py-2" onClick={() => setNavActive('user')}>User</Link>
                     </div>
                 </div>
 
